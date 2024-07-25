474bc2ef54759796b1ef7efb98c45ceed34c1452
SimonaMartinucci <simo.martinucci3@gmail.com>
2024-07-25 17:12:47 +0200

    build: main project file feat: add letters and numbers generators

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+
+$lunghezza = $_GET['number'];
+
+// generatore lettere maiuscole e minuscole
+function generatoreLettere($lunghezza) {
+    $lettereRandom = '';
+    for($i = 0; $i < $lunghezza; $i++) {
+        if (random_int(0, 1) === 1) {
+            $lettereRandom .= chr(random_int(65, 90));
+        } else {
+            $lettereRandom .= chr(random_int(97, 122));
+        }
+    }
+
+    return $lettereRandom;
+}
+
+// generatore numeri
+function generatoreNumeri($lunghezza) {
+    $numeriRandom = '';
+    for($i = 0; $i < $lunghezza; $i++) {
+        $numeriRandom .= random_int(0,9);
+    }
+
+    return $numeriRandom;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>php-strong-password-generator</title>
+</head>
+<body>
+    <h1>Strong Password Generator</h1>
+
+    <form action="index.php" method="GET">
+        <label for="number">Lunghezza password</label>
+        <input type="number" name="number" id="number">
+        <button type="submit">Genera</button>
+    </form>
+
+    <div><?php echo generatoreNumeri($lunghezza); ?></div>
+
+</body>
+</html>
